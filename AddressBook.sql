2cfdbf4de8088bc3d8de2a727789c9149f142ebf
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-14 15:16:06 +0530

    ADDED:Create table to create the table with coloumn names

--- AddressBook.sql
+++ AddressBook.sql
@@ -4,3 +4,18 @@ Create database AddressBookService
 
 --Use query is used to change the database which we are using
 use AddressBookService
+
+--UC2Create table in the addressbook database
+Create table Person(
+PersonId int identity(1,1) primary Key,
+Firstname varchar(50) Not Null,
+Lastname varchar(50) Not Null,
+Address varchar(50),
+City varchar(50),
+Zip int,
+MobileNumber varchar(10) unique,
+EmailId varchar(50)
+);
+
+
+
