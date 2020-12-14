c17f32c68e0cda129760e425f06a4eaec1087c44
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-14 15:04:57 +0530

    ADDED:Create query to create the database

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,6 @@
---Welcome to AddressBook
+
+--UC1 creating addressBook database
+Create database AddressBookService
+
+--Use query is used to change the database which we are using
+use AddressBookService
