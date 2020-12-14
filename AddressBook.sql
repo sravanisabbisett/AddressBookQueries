1ff97b4e195f4d16d12171b835436b231cf5d0e9
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-14 15:27:05 +0530

    ADDED:Insert query to insert the data in table

--- AddressBook.sql
+++ AddressBook.sql
@@ -5,6 +5,7 @@ Create database AddressBookService
 --Use query is used to change the database which we are using
 use AddressBookService
 
+
 --UC2Create table in the addressbook database
 Create table Person(
 PersonId int identity(1,1) primary Key,
@@ -17,5 +18,13 @@ MobileNumber varchar(10) unique,
 EmailId varchar(50)
 );
 
+--UC3_Insert values in to table
+Insert into Person(Firstname,Lastname,Address,City,Zip,MobileNumber,EmailId)
+values('Sravani','Sabbisetti','GandhiChowk','Bantumilli',521324,'8712443377','sravani.sabbisetti1@gmail.com'),
+('Rupika','Sabbisetti','Arunodayacolony','Hyderabad',500081,'9290815127','Rupika.sabbisetti@gmail.com'),
+('Manju','Chedhalla','Madhapur','Hyderabad',500081,'9712443377','ManjuChedhalla@gmail.com'),
+('Akhilesh','Sabbisetti','Perungudi','Chennai',567890,'7207321696','AkhileshSabbisetti@gmail.com'),
+('Saraiya','Purkana','Krths','Bantumilli',521324,'9959582197','Saraiaya@gmail.com');
 
-
+--Retrive all rows from person table
+Select * from Person;
